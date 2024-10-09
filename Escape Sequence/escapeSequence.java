7be633f2f9070bad7e3795aeecde23a27b2e9d1d
Talat09 <talat181008312@gmail.com>
2024-10-09 12:22:56 +0600

    done escape sequence

---
+++ Escape Sequence/escapeSequence.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class escapeSequence{
+    public static void main(String args[]){
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\tWorld");
+        System.out.println("Hello\bWorld");
+        System.out.println("Hello\rWorld");
+        System.out.println("Hello\fWorld");
+    
+
+        }
+}
