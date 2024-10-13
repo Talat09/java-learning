ab356f0977f703a9ed3de02b4edd2d785b193504
Talat09 <talat181008312@gmail.com>
2024-10-13 19:12:55 +0600

    complete pattern challenge using escape sequence

---
+++ Escape Sequence/Challenge/challengeOne.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class challengeOne{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println("\n* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.println("\n        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
