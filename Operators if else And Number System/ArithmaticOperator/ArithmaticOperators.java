ac6e6293edf7153ad104dcd9fe41436dc3d99177
Talat09 <talat181008312@gmail.com>
2024-10-22 20:58:07 +0600

    done Arithmatic Operators

---
+++ Operators if else And Number System/ArithmaticOperator/ArithmaticOperators.java
@@ -0,0 +1,20 @@
+public class ArithmaticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        System.out.println((int) a + b);
+        System.out.println((int) b - a);
+        System.out.println((int) a * b);
+        System.out.println((int) b / a);
+        System.out.println(c % b);
+        double x = 10.5;
+        double y = 20.5;
+        System.out.println(x + y);
+        System.out.println(y - x);
+        System.out.println(x * y);
+        System.out.println(y / x);
+        System.out.println(x % y);
+
+    }
+}
