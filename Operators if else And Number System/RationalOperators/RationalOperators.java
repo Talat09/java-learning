3811c9cd7131dc56457d146f618cc270b886d574
Talat09 <talat181008312@gmail.com>
2024-11-01 21:59:32 +0600

    done rational operators

---
+++ Operators if else And Number System/RationalOperators/RationalOperators.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+
+public class RationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a = " + a + ", b = " + b);
+
+        // == operator
+        System.out.println("a == b: " + (a == b)); // false
+
+        // != operator
+        System.out.println("a != b: " + (a != b)); // true
+
+        // > operator
+        System.out.println("a > b: " + (a > b)); // false
+
+        // < operator
+        System.out.println("a < b: " + (a < b)); // true
+
+        // >= operator
+        System.out.println("a >= b: " + (a >= b)); // false
+
+        // <= operator
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
